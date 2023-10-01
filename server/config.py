a4eabe08aa0dc26a361e47115d8fa83f4cacb332
Harpreet Singh <Harpreets1201@gmail.com>
2023-09-30 20:30:04 -0400

    Edits to config file

--- server/config.py
+++ server/config.py
@@ -1,4 +1,4 @@
-# config.py
+# Remote library imports
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
@@ -6,19 +6,23 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+# Instantiate app, set attributes
 app = Flask(__name__)
 app.secret_key = 'YOUR_APP_SECRET_HERE' # In the terminal, run: python -c 'import os; print(os.urandom(16))'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# instantiate db
 db = SQLAlchemy()
-
 migrate = Migrate(app, db)
 db.init_app(app)
 
+# instantiate Bcrypt
 bcrypt = Bcrypt(app)
 
+# Instantiate CORS
 CORS(app)
 
+# Instantiate REST API
 api = Api(app)
