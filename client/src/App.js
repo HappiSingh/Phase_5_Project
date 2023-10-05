cb9c2eb22be0453fedc55ab75597d2db163a4725
Harpreet Singh <Harpreets1201@gmail.com>
2023-10-05 03:08:09 -0400

    Getting started with frontend - App/BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>  Hello Sir</h1>
+      {/* <Routes>
+        <Route
+          path={"/productions/new"}
+          element={
+            <div>
+              <NewProductionForm addProduction={addProduction} />
+            </div>
+          }
+        />
+        <Route path={"/productions/:id"} element={<ProductionDetail />} />
+        <Route
+          path={"/authentication"}
+          element={
+            <div>
+              <Authentication updateUser={updateUser}/>
+            </div>
+          }
+        />
+        <Route
+          path={"/"}
+          element={
+            <div>
+              <Home productions={productions} />
+            </div>
+          }
+        />
+        <Route
+          path={"*"}
+          element={
+            <>
+              <h1>Sorry We can't find the Page you're looking for!</h1>
+              <h1>404 Not Found</h1>
+            </>
+          }
+        />
+      </Routes> */}
+    </>
   );
 }
 
