412a10304f392bca38d9349fc611d20f44830270
Harpreet Singh <Harpreets1201@gmail.com>
2023-10-06 23:29:05 -0400

    Added css to logout button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./navbar.css";
 
 function NavBar({ user, onLogout }) {
   if (!user) {
@@ -8,7 +9,7 @@ function NavBar({ user, onLogout }) {
       <>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand>Games_Review</Navbar.Brand>
+            <Navbar.Brand>Game_Reviews</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/login">Login</Nav.Link>
@@ -26,7 +27,9 @@ function NavBar({ user, onLogout }) {
             <Navbar.Brand>Games_Review</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <button onClick={onLogout}> Logout </button>
+              <button className="button" onClick={onLogout}>
+                Logout
+              </button>
             </Nav>
           </Container>
         </Navbar>
